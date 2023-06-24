e76d687b74f6b41c1d0599976ff0f837f8a48aa0
s3arajgupta <swaraj.gupta217@gmail.com>
2023-06-25 04:28:55 +0530

    test

--- 1_1480. Running Sum of 1d Array_mine.java
+++ 1_1480. Running Sum of 1d Array_mine.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] runningSum(int[] nums) {
 
-        // int sum = 0;
-
         for (int i = 1; i<nums.length; i++){
             
             nums[i] = nums[i-1] + nums[i];
