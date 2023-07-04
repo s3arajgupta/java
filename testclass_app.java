e172dd35622aebe9f0d6f3fb20d985973f1f5e05
s3arajgupta <swaraj.gupta217@gmail.com>
2023-07-05 02:57:08 +0530

    0507

---
+++ testclass_app.java
@@ -0,0 +1,11 @@
+public class testclass_app {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        testclass.testfuction();
+
+        // testclass y = new testclass();
+        // y.testfuction();
+
+    }
+}
