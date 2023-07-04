9e3fd02b7e6ce9117e8f83f2e198cac9342bec8e
s3arajgupta <swaraj.gupta217@gmail.com>
2023-07-05 05:24:19 +0530

    ganesh

--- testclass.java
+++ testclass.java
@@ -1,5 +1,3 @@
-
-
 public class testclass {
     public static void testfuction() throws Exception {
         System.out.println("Hello, testfuction from testclass :)!");
--- testclass_app.java
+++ testclass_app.java
@@ -1,5 +1,3 @@
-
-
 public class testclass_app {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
