5fa26311046349834ee3c3492ffa2f6fd6701fbb
s3arajgupta <swaraj.gupta217@gmail.com>
2023-07-10 01:37:16 +0530

    10 7

---
+++ LeetCode 75/1_Merge_Strings_Alternately.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void mergeAlternately(String word1, String word2) {
+
+        // System.out.println("Hello, mergeAlternately from Solution :)!");
+        // System.out.println(word1 + word2);
+
+        // int len1 = word1.length();
+        // int len2 = word2.length();
+        
+        
+        
+    }
+}
