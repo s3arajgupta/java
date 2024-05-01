4d24b129543d23577c54833456c04652f0d9eed3
s3arajgupta <swaraj.gupta217@gmail.com>
2024-05-01 15:52:05 +0530

    may day2

--- practice/neetCode/arrayAndHashing/medium/groupAnagrams.java
+++ practice/neetCode/arrayAndHashing/medium/groupAnagrams.java
@@ -127,3 +127,25 @@ public class groupAnagrams {
 //         };
 //     }
 // }
+
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         HashMap<String, List<String>> map = new HashMap<>();
+
+//         for (String str : strs) {
+
+//             char[] charArray = str.toCharArray();
+//             Arrays.sort(charArray);
+//             String sortedStr = new String(charArray);
+
+//             if (!map.containsKey(sortedStr)) {
+//                 map.put(sortedStr, new ArrayList<>());
+//             }
+
+//             map.get(sortedStr).add(str);
+//         }
+
+//         return new ArrayList<>(map.values());
+//     }
+// }
