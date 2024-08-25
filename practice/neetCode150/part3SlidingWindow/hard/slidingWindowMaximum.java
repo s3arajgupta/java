dd220ff2e534f63ad520a6b85741457d3d7094b1
s3arajgupta <swaraj.gupta217@gmail.com>
2024-08-25 06:25:54 +0530

    oka

--- practice/neetCode150/part3SlidingWindow/hard/slidingWindowMaximum.java
+++ practice/neetCode150/part3SlidingWindow/hard/slidingWindowMaximum.java
@@ -7,16 +7,11 @@ public class slidingWindowMaximum {
     public static void main(String args[]) {
 
         int[] arr = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
-        int[] res = maxSlidingWindow(arr, 4);
-        // int[] arr = new int[] { 1 };
-        // int[] res = maxSlidingWindow(arr, 1);
-        // int[] arr = new int[] { 1, -1 };
-        // int[] res = maxSlidingWindow(arr, 1);
-        for (int c : res)
-            System.out.print(c + " ");
+        System.out.println(Arrays.toString(maxSlidingWindow(arr, 4)));
 
     }
 
+    // optimal
     public static int[] maxSlidingWindow(int[] nums, int k) {
 
         Deque<Integer> deque = new LinkedList<>();
@@ -26,17 +21,22 @@ public class slidingWindowMaximum {
 
         while (r < n) {
 
+            // pop smaller values from the deque
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[r])
+                deque.removeLast();
+
+            deque.add(r);
+
+            // remove first value for out of bounds
             if (!deque.isEmpty() && deque.peekFirst() < r - k + 1)
                 deque.removeFirst();
 
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[r]) {
-                deque.removeLast();
+            // valid window
+            if (r + 1 >= k) {
+                res[l] = nums[deque.peekFirst()];
+                l++;
             }
-            deque.add(r);
 
-            if (r >= k - 1) {
-                res[l++] = nums[deque.peekFirst()];
-            }
             r++;
 
         }
@@ -47,28 +47,31 @@ public class slidingWindowMaximum {
 
     // public static int[] maxSlidingWindow(int[] nums, int k) {
 
-    // if (k == 0)
-    // return nums;
-    // if (k == 1)
-    // return nums;
+    //     if (k == 0)
+    //         return new int[] {};
 
-    // int[] res = new int[nums.length - k + 1];
+    //     if (k == 1)
+    //         return nums;
 
-    // HashMap<Integer, Integer> map = new HashMap<>();
-    // int l = 0;
-    // for (int r = 0; r < nums.length; r++) {
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     int[] res = new int[nums.length - k + 1];
+    //     int l = 0, r = 0;
 
-    // map.put(r, nums[r]);
+    //     while (r < nums.length) {
 
-    // if (r - l + 1 == k) {
-    // // System.out.println("if");
-    // res[l] = Collections.max(map.values());
-    // map.remove(l);
-    // l++;
-    // }
-    // }
+    //         map.put(r, nums[r]);
 
-    // return res;
+    //         if (r - l + 1 == k) {
+    //             res[l] = Collections.max(map.values());
+    //             map.remove(l);
+    //             l++;
+    //         }
+
+    //         r++;
+    //     }
+
+    //     return res;
+
     // }
 
 }
